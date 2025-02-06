8413f035b8dbac9964868df534ff75e764ada5cc
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-02-06 13:08:44 +0100

    Comando if implementado

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const passwordInput = document.getElementById('password');
 const message = document.getElementById('message');
 
+passwordInput.addEventListener('input', () => {
+    const passwordLength = passwordInput.value.length;
+    if (passwordLength < 8) {
+        message.textContent = 'Contraseña demasiado corta';
+        message.style.color = 'red';
+    } else if (passwordLength >= 8 && passwordLength <= 10) {
+        message.textContent = 'Contraseña intermedia';
+        message.style.color = 'yellow';
+    } else {
+        message.textContent = 'Contraseña segura';
+        message.style.color = 'green';
+    }
+});
