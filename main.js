fe7a01f44a2c8b142e2eb4e8a1170bda3240e960
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-02-06 13:50:49 +0100

    Simplificado y explicado

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
-function getPasswordInput() {
-    return document.getElementById('password');
-}
+// Obtener el input de contraseña y el elemento de mensaje
+const passwordInput = document.getElementById('password');
+const message = document.getElementById('message');
 
-function getMessageElement() {
-    return document.getElementById('message');
-}
-
+// Función para actualizar el mensaje según la longitud de la contraseña
 function updateMessage() {
-    const passwordLength = getPasswordInput().value.length
-    const message = getMessageElement();
+    const passwordLength = passwordInput.value.length;
+
     if (passwordLength < 8) {
         message.innerHTML = 'Contraseña demasiado corta';
         message.style.color = 'red';
     } else if (passwordLength >= 8 && passwordLength <= 10) {
-        message.textContent = 'Contraseña intermedia';
+        message.innerHTML = 'Contraseña intermedia';
         message.style.color = 'yellow';
     } else {
-        message.textContent = 'Contraseña segura';
+        message.innerHTML = 'Contraseña segura';
         message.style.color = 'green';
     }
 }
 
-function handlePasswordInput() {
-    const passwordInput = getPasswordInput();
-    passwordInput.addEventListener('keyup', updateMessage);
-}
-
-
-// Asignar el evento al input de la contraseña
-handlePasswordInput();
+// Asignar el evento 'keyup' para que actualice el mensaje cuando se escribe en el input
+passwordInput.addEventListener('keyup', updateMessage);
